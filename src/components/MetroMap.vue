77b5ed86cd9d82ebfb028d65af16ce8cb8a17b7a
Jaemin Lim <xsolute01@gmail.com>
2019-11-18 14:49:01 +0900

    MOD icon init algorithm

--- src/components/MetroMap.vue
+++ src/components/MetroMap.vue
@@ -13,110 +13,34 @@ export default {
     metroData: metro_data.default.seoulMetro
   }),
   methods: {
+    initIcon() {
+      let icons = [];
+    },
     initMap() {
       delete L.Icon.Default.prototype._getIconUrl;
-
       L.Icon.Default.mergeOptions({
         iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
         iconUrl: require("leaflet/dist/images/marker-icon.png"),
         shadowUrl: require("leaflet/dist/images/marker-shadow.png")
       });
 
-      const metroIcon = L.Icon.extend({
-        options: {
-          iconSize: [24, 24],
-          iconAnchor: [12, 12]
-        }
-      });
-
-      const line1Icon = new metroIcon({
-        iconUrl: require("@/assets/one.png")
-      });
-      const line2Icon = new metroIcon({
-        iconUrl: require("@/assets/two.png")
-      });
-      const line3Icon = new metroIcon({
-        iconUrl: require("@/assets/three.png")
-      });
-      const line4Icon = new metroIcon({
-        iconUrl: require("@/assets/four.png")
-      });
-      const line5Icon = new metroIcon({
-        iconUrl: require("@/assets/five.png")
-      });
-      const line6Icon = new metroIcon({
-        iconUrl: require("@/assets/six.png")
-      });
-      const line7Icon = new metroIcon({
-        iconUrl: require("@/assets/seven.png")
-      });
-      const line8Icon = new metroIcon({
-        iconUrl: require("@/assets/eight.png")
-      });
-      const line9Icon = new metroIcon({
-        iconUrl: require("@/assets/nine.png")
-      });
-      const lineBIcon = new metroIcon({
-        iconUrl: require("@/assets/boondang.png")
-      });
-      const lineSIcon = new metroIcon({
-        iconUrl: require("@/assets/sinboondang.png")
-      });
-
-      console.log(line1Icon);
-
       this.map = L.map("map", { doubleClickZoom: false });
 
       this.map.setView([37.5660649, 126.982373], 13);
 
       for (let station of this.metroData) {
-        let icon;
-        switch (station.line) {
-          case "1호선":
-            icon = line1Icon;
-            break;
-          case "2호선":
-            icon = line2Icon;
-            break;
-          case "3호선":
-            icon = line3Icon;
-            break;
-          case "4호선":
-            icon = line4Icon;
-            break;
-          case "5호선":
-            icon = line5Icon;
-            break;
-          case "6호선":
-            icon = line6Icon;
-            break;
-          case "7호선":
-            icon = line7Icon;
-            break;
-          case "8호선":
-            icon = line8Icon;
-            break;
-          case "9호선":
-            icon = line9Icon;
-            break;
-          case "분당선":
-            icon = lineBIcon;
-            break;
-          case "신분당선":
-            icon = lineSIcon;
-            break;
-          default:
-            break;
-        }
-        L.marker(
-          [parseFloat(station.longitude), parseFloat(station.latitude)],
-          { icon: icon, title: station.stationName, riseOnHover: true }
-        )
+        let icon = L.icon({
+          iconUrl: require(`../assets/${station.line}.png`),
+          iconSize: [20, 20]
+        });
+        L.marker([Number(station.longitude), Number(station.latitude)], {
+          icon: icon,
+          title: station.stationName,
+          riseOnHover: true
+        })
           .addTo(this.map)
           .bindPopup(station.stationName);
       }
-
-      // L.marker([37.566, 126.982]).addTo(this.map);
 
       this.tileLayer = L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
         maxZoom: 18
